47509b89e9211e494e858d9cac0986ef3796da33
Paulo Orquillo <olopsman@users.noreply.github.com>
2022-05-29 21:02:58 +1200

    Initial Commit

---
+++ TimelineView/TimelineViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimelineViewApp.swift
+//  TimelineView
+//
+//  Created by Paulo Orquillo on 29/05/22.
+//
+
+import SwiftUI
+
+@main
+struct TimelineViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
